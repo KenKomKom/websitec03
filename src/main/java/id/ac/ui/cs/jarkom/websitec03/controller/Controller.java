27d98a8540edb9bdd5b3520a434442b69b4c0544
KenKomKom <kenichikomala@gmail.com>
2024-12-26 23:38:08 +0700

    uwu6

--- src/main/java/id/ac/ui/cs/jarkom/websitec03/controller/Controller.java
+++ src/main/java/id/ac/ui/cs/jarkom/websitec03/controller/Controller.java
@@ -1,19 +1,13 @@
 package id.ac.ui.cs.jarkom.websitec03.controller;
 
-import id.ac.ui.cs.jarkom.websitec03.model.Company;
-import id.ac.ui.cs.jarkom.websitec03.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
 public class Controller {
-    @Autowired
-    private Repository repo;
     String homeHTML = "home";
 
     @GetMapping("")
@@ -32,13 +26,4 @@ public class Controller {
 
         return getModelAndView(homeHTML);
     }
-
-    @GetMapping("/savesql")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Company addCompany()
-    {
-        Company c = new Company("Budi", 100, "lala",1000000, false);
-        return repo.save(c);
-    }
-
 }
